36b2c72b1479e437ff81bdd79b30a3218d92056f
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-24 11:00:58 +0530

    [Akshit;add;uc1 case cal length of line]

--- linecomparison.cpp
+++ linecomparison.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
     cout<<"Line comparison Problem"<<endl;
+
+    int x1,x2,y1,y2;
+    cin>>x1>>x2>>y1>>y2;
+    int ans=(sqrt((x2-x1)*(x2-x1))) -sqrt((y2-y1)*(y2-y1));
+    cout<<ans;
 }
