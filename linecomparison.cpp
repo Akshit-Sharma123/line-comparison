8a6cefac1db04a2f8098b9562c9ea63ae1e7f47a
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-24 11:15:51 +0530

    [Akshit;add;UC2 checking if two lengths of line are equal]

--- linecomparison.cpp
+++ linecomparison.cpp
@@ -1,11 +1,31 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-int main(){
-    cout<<"Line comparison Problem"<<endl;
+int calLengthOfLine(){
 
     int x1,x2,y1,y2;
-    cin>>x1>>x2>>y1>>y2;
+    cout<<"Enter co ordinates of a line "<<endl;
+    cout<<"Enter x1 "<<endl;
+    cin>>x1;
+    cout<<"Enter x2 "<<endl;
+    cin>>x2;
+    cout<<"Enter y1 "<<endl;
+    cin>>y1;
+    cout<<"Enter y2 "<<endl;
+    cin>>y2;
     int ans=(sqrt((x2-x1)*(x2-x1))) -sqrt((y2-y1)*(y2-y1));
-    cout<<ans;
+    return ans;
+}
+int main(){
+    cout<<"Line comparison Problem"<<endl;
+    
+    
+    int line1length=calLengthOfLine();
+    int line2length=calLengthOfLine();
+    if(line1length==line2length){
+        cout<<"Two lines are equal"<<endl;
+    }
+    else{
+      cout<<"Two lines are not equal"<<endl;
+    }
 }
